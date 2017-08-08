bd74ef89c25618e3d64a188d64e928a555f80d38
Pascal Martineau <pascal@lewebsimple.ca>
2017-08-08 11:47:14 -0400

    Initial ACF field starter renamed to socialmedia

---
+++ acf-socialmedia.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Plugin Name: Advanced Custom Fields: Social Media
+Plugin URI: https://gitlab.ledevsimple.ca/wordpress/acf-socialmedia
+Description: Social Media field for ACF.
+Version: 0.1.0
+Author: Pascal Martineau <pascal@lewebsimple.ca>
+Author URI: https://lewebsimple.ca
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'acf_plugin_socialmedia' ) ) :
+
+	class acf_plugin_socialmedia {
+
+		function __construct() {
+			$this->settings = array(
+				'version' => '0.1.0',
+				'url'     => plugin_dir_url( __FILE__ ),
+				'path'    => plugin_dir_path( __FILE__ )
+			);
+
+			load_plugin_textdomain( 'acf-socialmedia', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+
+			add_action( 'acf/include_field_types', array( $this, 'include_field_types' ) );
+		}
+
+		function include_field_types( $version = false ) {
+			if ( ! $version ) {
+				$version = 5;
+			}
+			include_once( 'fields/acf-socialmedia-v' . $version . '.php' );
+		}
+
+	}
+
+	new acf_plugin_socialmedia();
+
+endif;
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Advanced Custom Fields: Social Media ===
+Contributors: Pascal Martineau <pascal@lewebsimple.ca>
+Tags: acf, socialmedia, field
+Requires at least: 3.5
+Tested up to: 3.8.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Social Media field for ACF.
+
+= Compatibility =
+
+This ACF field type is compatible with:
+* ACF 5
+
+== Installation ==
+
+1. Copy the `acf-socialmedia` folder into your `wp-content/plugins` folder
+2. Activate the Advanced Custom Fields: Social Media plugin via the plugins admin page
+3. Create a new field via ACF and select the Social Media type
+4. Please refer to the description for more info regarding the field type settings
+
+== Changelog ==
+
+= 0.1.0 =
+* Initial Release.
